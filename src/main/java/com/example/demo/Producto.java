4065c45f8de8f03181b63e06416b92111da1eac3
Valerio <ortizvalerio613@gmail.com>
2026-02-17 18:18:58 -0400

    ñadiendo tablas de productos

---
+++ src/main/java/com/example/demo/Producto.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nombre;
+    private Double precio;
+    private String imagenUrl;
+}
