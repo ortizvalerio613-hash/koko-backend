07ea9281963686fcffbfc113dd8f8f81898f3df1
Valerio <ortizvalerio613@gmail.com>
2026-02-17 18:30:53 -0400

    Añadido controlador para guardar productos

---
+++ src/main/java/com/example/demo/ProductoController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/productos")
+@CrossOrigin(origins = "*") // Para que tu React pueda conectarse
+public class ProductoController {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @GetMapping
+    public List<Producto> obtenerTodos() {
+        return productoRepository.findAll();
+    }
+
+    @PostMapping
+    public Producto guardar(@RequestBody Producto producto) {
+        return productoRepository.save(producto);
+    }
+}
